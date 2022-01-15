e24cab3a5c45d2d8bc80d8f53b49d66a8ad73c42
Randall Thomas <randallthomasmusic@gmail.com>
2022-01-15 08:35:14 -0800

    init commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// ---------STEP TWO -------------
+// write function that finds weekly income - (income times 7). The function will take an animal object as an argument
+
+const weeklyIncome = () => {
+   
+}
+// ---------------STEP THREE----------------
+// write function that takes two arguments, one animal object and the weekly income variable
+// compare if weekly income is greater than dollarsSpentOnFoodPerWeek
+// if true, add a keep: true value to object, if not- keep: false
+
+
+const worthIt = () => {
+    
+}
+
+// ---------READ ME --------------
+// Zoo keeper needs to cut some costs. Find which animals need to get the chop
+const zooAnimals = [
+    { name: "bear", dollarsSpentOnFoodPerWeek: 100, incomePerDay: 90 },
+    { name: "lion", dollarsSpentOnFoodPerWeek: 100, incomePerDay: 15 },
+    { name: "penguine", dollarsSpentOnFoodPerWeek: 40, incomePerDay: 6 },
+    { name: "wolf", dollarsSpentOnFoodPerWeek: 70, incomePerDay: 9 },
+    { name: "babadook", dollarsSpentOnFoodPerWeek: 700, incomePerDay: 13 }
+]
+
+// ---------STEP ONE ---------
+// write a loop that iterates through the array, then write your functions above
+// (complete the for loop after functions are written above)
---
+++ practice.js
@@ -0,0 +1,30 @@
+// -------STEP TWO ----------
+// Make a function that takes one object and 
+// multiply hourly property by hoursPerWeek property
+const dollarsPerWeek = () => {
+    
+}
+
+// ------STEP THREE ---------
+// Make a function that takes two arguments (variables) 
+// one will be the employee object, the other dollarsperweek
+// If dollarsperweek is greater than 150, add property to object
+// gettingPaid = true, if not, gettingPaid = false
+const gettingPaid = () => {
+    
+}
+
+// ------------------READ ME FIRST--------------------------
+// owner of restaurant is getting raise requests and wants to see 
+// who out of his employees are getting paid "enough"
+const employeeList = [
+    {name: "kim", hourly: 4.50, dailyTips: 150, hoursPerWeek: 40},
+    {name: "maud", hourly: 6.00, dailyTips: 100, hoursPerWeek: 30},
+    {name: "ricky", hourly: 2.50, dailyTips: 80, hoursPerWeek: 20},
+    {name: "flo", hourly: 5.00, dailyTips: 150, hoursPerWeek: 38},
+    {name: "damian", hourly: 8.00, dailyTips: 50, hoursPerWeek: 15}
+]
+
+// ------------- STEP ONE ----------
+// iterate through the array of employees 
+ 
